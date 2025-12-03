bfd7770cdff8ccab3da1b70f65e1ae34929d18c4
Konstantinos Theofilis <theofilis@mac.com>
2025-12-03 18:45:04 +0900

    Fix bash script

--- setup.bash
+++ setup.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/llvm-10/lib
+export CMAKE_CXX_COMPILER=/usr/lib/llvm-10/bin/clang++
 pip install haros-plugin-pbt-gen
 cd haros && python3 setup.py install
 haros init
-cp configs.yaml ./haros/
+cp haros/configs.yaml /root/.haros/configs.yaml
 cd ../turtlebot3
 rosdep install --from-paths src --ignore-src
 catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+source devel/setup.bash
